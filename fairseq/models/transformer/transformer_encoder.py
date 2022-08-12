fc1fabc8612cd25cf3e15a5623ebddd59f1219bd
EIFY <chuanchih@gmail.com>
2022-08-11 17:18:06 -0700

    decoder alibi

--- fairseq/models/transformer/transformer_encoder.py
+++ fairseq/models/transformer/transformer_encoder.py
@@ -102,32 +102,9 @@ class TransformerEncoderBase(FairseqEncoder):
         else:
             self.layer_norm = None
 
-        def get_slopes(n):
-            def get_slopes_power_of_2(n):
-                start = (2**(-2**-(math.log2(n)-3)))
-                ratio = start
-                return [start*ratio**i for i in range(n)]
-
-            if math.log2(n).is_integer():
-                return get_slopes_power_of_2(n)                   #In the paper, we only train models that have 2^a heads for some a. This function has
-            else:                                                 #some good properties that only occur when the input is a power of 2. To maintain that even
-                closest_power_of_2 = 2**math.floor(math.log2(n))  #when the number of heads is not a power of 2, we use this workaround. 
-                return get_slopes_power_of_2(closest_power_of_2) + get_slopes(2*closest_power_of_2)[0::2][:n-closest_power_of_2]
-
         if cfg.encoder.alibi:
             assert self.embed_positions is None, "ALiBi shouldn't be used with positional embedding"
-            maxpos = self.max_source_positions
-            attn_heads = cfg.encoder.attention_heads
-
-            context_position = torch.arange(maxpos)[:, None]
-            memory_position = torch.arange(maxpos)[None, :]
-            relative_position = torch.abs(memory_position - context_position).float()
-            if cfg.encoder.alibi_asymmetrical:
-                relative_position -= 0.5 * torch.triu(torch.ones(maxpos, maxpos), diagonal=1)
-            relative_position = relative_position.unsqueeze(0).expand(attn_heads, -1,-1)        
-
-            self.slopes = torch.Tensor(get_slopes(attn_heads)) * -1
-            self.alibi = self.slopes.unsqueeze(1).unsqueeze(1) * relative_position
+            self.alibi = utils.alibi(cfg.encoder.attention_heads, self.max_source_positions, cfg.encoder.alibi_asymmetrical)
         else:
             self.alibi = None
 
